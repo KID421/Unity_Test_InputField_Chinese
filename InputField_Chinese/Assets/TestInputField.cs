4ab93424426b236e54310fa5c268576730579eac
KID <50666207+KID421@users.noreply.github.com>
2020-05-18 19:31:31 +0800

    網頁版

---
+++ InputField_Chinese/Assets/TestInputField.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestInputField : MonoBehaviour
+{
+    public Text result;
+
+    public void CheckInput(string input)
+    {
+        if (input == "紅水")
+        {
+            result.text = "恢復血量";
+        }
+        else if (input == "藍水")
+        {
+            result.text = "恢復魔力";
+        }
+    }
+}
